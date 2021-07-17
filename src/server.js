26b2c2c9a7400ff7d63f7a6aa0fec2805c0f3bee
OT13 <omertahir13@gmail.com>
2021-07-17 13:46:51 +0800

    Added simple git and post requests handling

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 import express from "express";
 
+/* 
+Was initially used to parse the body but is now deprecated.
+We'll now use the method of the same name under the express object, `express.json()`
+import bodyParser from "body-parser";
+ */
+
 const app = express();
+app.use(express.json());
 
 app.get('/hello',(req,res) => res.send('Hello!'));
+app.get('/hello/:name',(req,res) => res.send(`Hello ${req.params.name}`));
+app.post('/hello',(req,res) => res.send(`Hello ${req.body.name}!`));
 app.listen(8000,() => console.log('Server is listening on port 8000'));
