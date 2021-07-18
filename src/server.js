8c1b6a9e810a65e5467e2bfc58862581dda37881
OT13 <omertahir13@gmail.com>
2021-07-18 20:46:15 +0800

    Refactored the upvote function to use the DB values

--- src/server.js
+++ src/server.js
@@ -30,18 +30,37 @@ app.get('/api/articles/:name', async (req,res) => {
 
 })
 
-app.post('/api/articles/:name/upvote',(req,res) => {
-    const articleName = req.params.name;
+app.post('/api/articles/:name/upvote', async (req,res) => {
+    try
+    {
+        const articleName = req.params.name;
+    
+        const client = await MongoClient.connect('mongodb://localhost:27017',{useNewUrlParser: true})
+        const db = client.db('my-blog');
+    
+        const articleInfo = await db.collection('articles').findOne({name: articleName});
+        await db.collection('articles').updateOne({name: articleName}, {
+            '$set' : {
+                upvotes: articleInfo.upvotes + 1
+            }
+        });
 
-    articlesInfo[articleName].upvotes += 1;
-    res.status(200).send(`${articleName} currently has ${articlesInfo[articleName].upvotes} upvotes!`);
+        const updatedArticleInfo = await db.collection('articles').findOne({name: articleName});
+        res.status(200).json(updatedArticleInfo);
+
+        client.close();
+
+    } catch(error) {
+        res.status(500).json({ message: 'Error connecting to the DB', error});
+    }
+
 })
 
 app.post('/api/articles/:name/add-comment',(req,res) => {
     const articleName = req.params.name;
     const {username, text} = req.body;
 
-    articlesInfo[articleName].comments.push({username, text}); 
+    // articlesInfo[articleName].comments.push({username, text}); 
 
     res.status(200).send(articlesInfo[articleName]);
 })
